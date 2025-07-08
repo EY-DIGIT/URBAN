7a1840d6bfdf29fd7513e90aba911be3d4c7b210
EY-DIGIT <manish.1@in.ey.com>
2025-07-08 12:08:03 +0530

    Update V20250708113930__alter_eg_pt_unit_ddl.sql file
    Update V20250708113930__alter_eg_pt_unit_ddl.sql file

--- property-tax/property-services/src/main/resources/db/migration/main/V20250708113930__alter_eg_pt_unit_ddl.sql
+++ property-tax/property-services/src/main/resources/db/migration/main/V20250708113930__alter_eg_pt_unit_ddl.sql
@@ -1,2 +1,2 @@
-ALTER TABLE public.eg_pt_unit RENAME COLUMN fromDate to fromYear;
-ALTER TABLE public.eg_pt_unit RENAME COLUMN toDate to toYear;
+ALTER TABLE public.eg_pt_unit RENAME COLUMN fromDate To fromYear;
+ALTER TABLE public.eg_pt_unit RENAME COLUMN toDate To toYear;
